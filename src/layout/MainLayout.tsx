2140dda0fa98a1a6b33cb7b426e17c8b5d98f471
leopaul29 <leopaulmartin.iut@gmail.com>
2025-09-27 23:44:25 +0900

    update mainLayout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { TabStrip, TabStripTab } from "@progress/kendo-react-layout";
+import {TabStrip, type TabStripSelectEventArguments, TabStripTab} from "@progress/kendo-react-layout";
 import { useState } from "react";
 import { Dashboard } from "../pages/Dashboard";
 import { Missions } from "../pages/Missions";
@@ -9,31 +9,32 @@ import { Profile } from "../pages/Profile";
 export const MainLayout = () => {
     const [selected, setSelected] = useState(0);
 
+    const handleSelect = (e: TabStripSelectEventArguments) => {
+        setSelected(e.selected);
+    }
+    const position = "top";
+    const alignment = "stretched";
+
     return (
-        <TabStrip
-            selected={selected}
-            onSelect={(e) => setSelected(e.selected)}
-            animation={true}
-        >
-            <TabStripTab title="Dashboard">
-                {selected === 0 && <Dashboard />}
-            </TabStripTab>
+        <>
+            <TabStrip selected={selected} onSelect={handleSelect} animation={true} tabPosition={position} tabAlignment={alignment}>
+                <TabStripTab title="Dashboard">
+                    {selected === 0 && <Dashboard />}
+                </TabStripTab>
+                <TabStripTab title="Missions">
+                    {selected === 1 && <Missions />}
+                </TabStripTab>
+                <TabStripTab title="Stats">
+                    {selected === 2 && <Stats />}
+                </TabStripTab>
+                <TabStripTab title="Shop">
+                    {selected === 3 && <Shop />}
+                </TabStripTab>
+                <TabStripTab title="Profile">
+                    {selected === 4 && <Profile />}
+                </TabStripTab>
+            </TabStrip>
 
-            <TabStripTab title="Missions">
-                {selected === 1 && <Missions />}
-            </TabStripTab>
-
-            <TabStripTab title="Stats">
-                {selected === 2 && <Stats />}
-            </TabStripTab>
-
-            <TabStripTab title="Shop">
-                {selected === 3 && <Shop />}
-            </TabStripTab>
-
-            <TabStripTab title="Profile">
-                {selected === 4 && <Profile />}
-            </TabStripTab>
-        </TabStrip>
+        </>
     );
 };
