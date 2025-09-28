b46af6653a398207dd79db1d5f4e9e2dcdebc416
leopaul29 <leopaulmartin.iut@gmail.com>
2025-09-28 14:25:45 +0900

    import kendo theme + i18next config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+
+import '@progress/kendo-theme-fluent/dist/all.css';
+
+// IMPORTANT: Import i18n config BEFORE any components that use translations
+import './i18n/config';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
