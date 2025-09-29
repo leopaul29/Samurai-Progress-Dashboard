48047bbe46f2c9a0f29499e7cfd8d07e8124b349
leopaul29 <leopaulmartin.iut@gmail.com>
2025-09-29 11:07:01 +0900

    smaller max-width and center content

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import {Missions} from "./pages/Missions.tsx";
 import {Stats} from "./pages/Stats.tsx";
 import {Shop} from "./pages/Shop.tsx";
 import {Profile} from "./pages/Profile.tsx";
+import './App.css';
 
 function App() {
     return (
-        <BrowserRouter >
-            <Header />
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/missions" element={<Missions />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/stats" element={<Stats />} />
-                <Route path="/profile" element={<Profile />}/>
-            </Routes>
-        </BrowserRouter >
+        <div className="app">
+            <BrowserRouter >
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/missions" element={<Missions />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/stats" element={<Stats />} />
+                    <Route path="/profile" element={<Profile />}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './main.css'
 
 import '@progress/kendo-theme-fluent/dist/all.css';
 import {GameProvider} from "./contexts/GameContext.tsx";
